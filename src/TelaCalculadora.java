b5f54140de7e532dd7ff16e9d34114426b53219c
Renata Mayra <renata62514466@alunos.sc.senac.br>
2026-06-16 22:00:26 -0300

    divisao

--- src/TelaCalculadora.java
+++ src/TelaCalculadora.java
@@ -133,7 +133,10 @@ txtresultado.setText(String.valueOf(resultado));
     }//GEN-LAST:event_btnmultiplicarActionPerformed
 
     private void btndividirActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btndividirActionPerformed
-        
+double numero1 = Double.parseDouble(txtprimeiro.getText());
+double numero2 = Double.parseDouble(txtsegundo.getText());
+double resultado = numero1 / numero2;
+txtresultado.setText(String.valueOf(resultado));        
     }//GEN-LAST:event_btndividirActionPerformed
 
     
